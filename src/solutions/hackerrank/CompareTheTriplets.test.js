040fc5b4d2940acffce43f81f4708b79fc20c012
ahribori <ahribori@gmail.com>
2019-05-28 00:51:33 +0900

    해커랭크 - CompareTheTriplets

---
+++ src/solutions/hackerrank/CompareTheTriplets.test.js
@@ -0,0 +1,18 @@
+const solution = (a, b) => {
+  const result = [0, 0];
+
+  for (let i = 0; i < 3; i++) {
+    if (a[i] > b[i]) {
+      result[0]++;
+    } else if (a[i] < b[i]) {
+      result[1]++;
+    }
+  }
+
+  return result;
+};
+
+test('solution', () => {
+  expect(solution([5, 6, 7], [3, 6, 10])).toStrictEqual([1, 1]);
+  expect(solution([17, 28, 30], [99, 16, 8])).toStrictEqual([2, 1]);
+});
