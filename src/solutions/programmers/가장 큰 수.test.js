ebe8e722aa0916692043d3819a706b6124a03235
ahribori <ahribori@gmail.com>
2019-07-09 00:48:59 +0900

    가장 큰 수

--- src/solutions/programmers/가장 큰 수.test.js
+++ src/solutions/programmers/가장 큰 수.test.js
@@ -1,7 +1,16 @@
 function solution(numbers) {
+  const answer = numbers
+    .map(item => item.toString())
+    .sort((a, b) => {
+      return parseInt(b + a) - parseInt(a + b);
+    })
+    .join('');
+
+  return answer.startsWith('0') ? '0' : answer;
 }
 
 test('solution', () => {
   expect(solution([6, 10, 2])).toBe('6210');
   expect(solution([3, 30, 34, 5, 9])).toBe('9534330');
+  expect(solution([0, 0, 0, 0])).toBe('0');
 });
