25a24acb8a0d55796a2a2f6614ad2752b5b86cbb
ahribori <ahribori@gmail.com>
2020-04-13 00:27:38 +0900

    DiameterOfBinaryTree.js

--- solutions/leetcode/DiameterOfBinaryTree.js
+++ solutions/leetcode/DiameterOfBinaryTree.js
@@ -3,9 +3,21 @@ function TreeNode(val) {
   this.left = this.right = null;
 }
 
-let count = 0;
-function diameterOfBinaryTree(node) {
-  // TODO
+function getHeight(root) {
+  return root ? 1 + Math.max(getHeight(root.left), getHeight(root.right)) : 0;
+}
+
+function diameterOfBinaryTree(root) {
+  if (root) {
+    const leftHeight = getHeight(root.left);
+    const rightHeight = getHeight(root.right);
+    return Math.max(
+      leftHeight + rightHeight,
+      diameterOfBinaryTree(root.left),
+      diameterOfBinaryTree(root.right),
+    );
+  }
+  return 0;
 }
 
 const tree = new TreeNode(1);
@@ -13,3 +25,6 @@ tree.left = new TreeNode(2);
 tree.right = new TreeNode(3);
 tree.left.left = new TreeNode(4);
 tree.left.right = new TreeNode(5);
+
+const diameter = diameterOfBinaryTree(tree);
+console.log(diameter);
