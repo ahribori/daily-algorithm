0cb133d75c3d1069028aa19b0a22a6ca3ea7ef99
daniel.hs <daniel.hs@kakaocorp.com>
2020-07-21 15:58:33 +0900

    Reverse String.js

---
+++ solutions/leetcode/Reverse String.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+const reverseString = function(s) {
+  return s.reverse();
+};
+
+reverseString(['h', 'e', 'l', 'l', 'o']);
