e21c14aad3f9cdd63fafc1e78491fb8a412bc4cd
daniel.hs <daniel.hs@kakaocorp.com>
2020-07-23 17:51:59 +0900

    Convert Sorted Array to Binary Search Tree.js

--- solutions/leetcode/helper/index.js
+++ solutions/leetcode/helper/index.js
@@ -3,6 +3,12 @@ function ListNode(val, next) {
   this.next = next === undefined ? null : next;
 }
 
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
 function createLinkedListFromArray(array) {
   if (!array || array.length === 0) {
     return null;
@@ -31,7 +37,30 @@ function printLinkedList(node) {
   }
 }
 
+function createBSTFromArray(nums) {
+  if (nums.length === 0) return null;
+  let root;
+
+  function create(items, parent) {
+    if (items.length === 0) return null;
+    const midIndex = Math.floor((items.length - 1) / 2);
+    const mid = new TreeNode(items[midIndex]);
+    if (!root) root = mid;
+    const leftItems = items.slice(0, midIndex);
+    const rightItems = items.slice(midIndex + 1, items.length);
+    if (parent) {
+      parent[parent.val > mid.val ? 'left' : 'right'] = mid;
+    }
+    create(leftItems, mid);
+    create(rightItems, mid);
+  }
+  create(nums, null);
+
+  return root;
+}
+
 module.exports = {
   createLinkedListFromArray,
+  createBSTFromArray,
   printLinkedList,
 };
